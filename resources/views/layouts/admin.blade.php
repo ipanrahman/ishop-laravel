fde33c1798da7f17e90e26c3578fcd0781c16da9
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2019-07-09 23:23:25 +0700

    add ide-helper

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -14,61 +14,59 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <link rel="stylesheet" type="text/css"
-        href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+          href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link href="{{ asset('css/all.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('pages/admin/style.css') }}">
-    @include('layouts.styles')
-
 </head>
 
 <body>
-    <div class="wrapper">
-        <!-- Sidebar -->
-        <nav id="sidebar">
-            <div class="sidebar-header">
-                <h3>{{ config('app.name') }}</h3>
-            </div>
+<div class="wrapper">
+    <!-- Sidebar -->
+    <nav id="sidebar">
+        <div class="sidebar-header">
+            <h3>{{ config('app.name') }}</h3>
+        </div>
 
-            <ul class="list-unstyled components">
-                <li class="active">
-                    <a href="javascript:void(0)">Home</a>
-                <li>
-                    <a href="{{ route('admin.products.index') }}">Product</a>
-                </li>
-                <li>
-                    <a href="{{ route('admin.orders.index') }}">Order</a>
-                </li>
-                <li>
-                    <a href="#">Cart</a>
-                </li>
-            </ul>
+        <ul class="list-unstyled components">
+            <li class="active">
+                <a href="javascript:void(0)">Home</a>
+            <li>
+                <a href="{{ route('admin.products.index') }}">Product</a>
+            </li>
+            <li>
+                <a href="{{ route('admin.orders.index') }}">Order</a>
+            </li>
+            <li>
+                <a href="#">Cart</a>
+            </li>
+        </ul>
 
-        </nav>
-        <!-- Page Content -->
-        <div id="content">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ml-auto">
-                            <!-- Authentication Links -->
-                            @guest
+    </nav>
+    <!-- Page Content -->
+    <div id="content">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
                             <li class="nav-item">
                                 <a style="margin-right:10px" class="nav-link" href="{{ route('login') }}"><i
                                         class=""></i> {{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link " href="{{ route('register') }}"><i class=""></i>
-                                    {{ __('Register') }}</a>
-                            </li>
+                                <li class="nav-item">
+                                    <a class="nav-link " href="{{ route('register') }}"><i class=""></i>
+                                        {{ __('Register') }}</a>
+                                </li>
                             @endif
-                            @else
+                        @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle " href="#" role="button"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre><i
+                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre><i
                                         class="fa fa-user"></i>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
@@ -80,35 +78,20 @@
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                        style="display: none;">
+                                          style="display: none;">
                                         @csrf
                                     </form>
                                 </div>
                             </li>
-                            @endguest
-                        </ul>
-                    </div>
+                        @endguest
+                    </ul>
                 </div>
-            </nav>
-            @yield('content')
-        </div>
+            </div>
+        </nav>
+        @yield('content')
     </div>
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script
-        src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js">
-    </script>
-    <script>
-        $(document).ready(function () {
-        $("#sidebar").mCustomScrollbar({
-            theme: "minimal"
-        });
-        $('#sidebarCollapse').on('click', function () {
-            $('#sidebar').toggleClass('active');
-        });
-    });
-    </script>
-    @include('layouts.script')
-    @stack('scripts')
+</div>
+<script src="{{ asset('js/app.js') }}"></script>
+@stack('scripts')
 </body>
-
 </html>
